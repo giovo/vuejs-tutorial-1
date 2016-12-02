b80a532379e8e1daa8f6cb920d9d891a5154822b
gio <gio@isdev.net>
2016-12-02 12:53:35 -0500

    started tutorial

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+
+new Vue({
+  // Target div with id = 'events'
+  el: '#events'
+
+  // Register any values or collections that hold data for the app
+  data: {},
+
+  // Anything within the ready function will run when the app loads
+  ready: function() {},
+
+  // Methods we want to use in our app
+  methods: {}
+
+});
+
+
